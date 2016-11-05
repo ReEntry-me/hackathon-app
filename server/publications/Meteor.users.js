baec9ef847b4b504fa17ea08600427cb6304e414
Dan Rubins <dan@legalrobot.com>
2016-11-05 11:20:18 -0700

    Add Meteor.users schema, remove sensitive info, stop over-publishing users

--- server/publications/Meteor.users.js
+++ server/publications/Meteor.users.js
@@ -1,6 +1,21 @@
-// TODO: call this in entry file
-export default function () {
-  Meteor.publish('Meteor.users', function () {
-    return Meteor.users.find();
-  });
+export default function() {
+    Meteor.publish('Meteor.users', function() {
+        if (this.userId) {
+            if (Roles.userIsInRole(this.userId, 'admin')) {
+                return Meteor.users.find({}, {
+                    fields: {
+                        services: 0
+                    }
+                });
+            } else {
+                return Meteor.users.find({ _id: this.userId }, {
+                    fields: {
+                        services: 0
+                    }
+                });
+            }
+        }
+
+        return this.ready();
+    });
 }
--- universal/models/Meteor.users.js
+++ universal/models/Meteor.users.js
@@ -1,17 +1,86 @@
+const emailsSchema = new SimpleSchema({
+    address: {
+        type: String,
+        label: 'Address',
+        optional: false,
+        regEx: SimpleSchema.RegEx.Email,
+        min: 3,
+        max: 100
+    },
+    verified: {
+        type: Boolean,
+        label: 'Verified',
+        optional: false,
+        defaultValue: false
+    }
+});
+
+const profileSchema = new SimpleSchema({
+    firstName: {
+        type: String,
+        label: 'First Name',
+        optional: false
+    },
+    lastName: {
+        type: String,
+        label: 'Last Name',
+        optional: false
+    },
+    jobTitle: {
+        type: String,
+        label: 'Job Title',
+        optional: true
+    },
+    company: {
+        type: String,
+        label: 'Company',
+        optional: true
+    },
+    bio: {
+        type: String,
+        label: 'Bio',
+        optional: true
+    }
+});
 
 Meteor.users.attachSchema(
     new SimpleSchema({
-    title: {
-      type: String
-    },
-    content: {
-      type: String
-    },
-    createdAt: {
-      type: Date,
-      denyUpdate: true
-    }
-  })
+        emails: {
+            type: emailsSchema,
+            label: 'Emails',
+            optional: true
+        },
+        profile: {
+            type: profileSchema,
+            label: 'Profile',
+            optional: true
+        },
+        services: {
+            type: Object,
+            label: 'Services',
+            optional: true,
+            blackbox: true
+        },
+        roles: {
+            type: [String],
+            label: 'Roles',
+            optional: true,
+            blackbox: true
+        },
+        createdAt: {
+            type: Date,
+            denyUpdate: true,
+            autoValue: function() {
+                if (this.isInsert) {
+                    return new Date();
+                } else if (this.isUpsert) {
+                    return {
+                        $setOnInsert: new Date()
+                    };
+                }
+            }
+        }
+    })
 );
 
 // Collection2 already does schema checking
